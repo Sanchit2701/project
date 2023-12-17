3763e2a9f04baf35ba9af25cc9bfbe271f23f901
Sanchit Grover <95961685+Sanchit2701@users.noreply.github.com>
2023-12-18 03:09:51 +0530

    Update insertion sort.py

--- insertion sort.py
+++ insertion sort.py
@@ -1,14 +1,30 @@
-# Calculate Returns
-Returns = RawStockDataPivot.copy()
-Returns[StockTickers] = Returns[StockTickers] / Returns[StockTickers].shift(12) - 1
-Returns = Returns.dropna()
+# Evaluation Function
+def evalFunction(Weights, evalGoal='risk'):
+    # Calculate risk and Sharpe Ratio based on evaluation function
+    risk = evalRisk(MONTHS_IN_YEAR, Weights, VarCov, ExpectedReturns, minDesiredReturn)
+    sharpeRatio = evalExpectedReturn(MONTHS_IN_YEAR, Weights, ExpectedReturns)
+    fitnessValue = risk if evalGoal == 'risk' else -sharpeRatio
+    return fitnessValue
 
-# Calculate Expected Returns
-ExpectedReturns = Returns.mean()
+# Initialization and Evaluation
+def initializeSwarm(numPorfolios, numStocksInPort, functionToGetBest, evalGoal, absoluteMoveLimit):
+    # Initialize portfolios, velocities, and fitness values
+    # Log the best positions and fitness values
+    return [Weights, Velocity, pCurrFitValue, pBestPosition, pBestFitValue, glBestFitValue, glBestPosition]
 
-# Calculate Excess Returns
-ExcessReturns = Returns - ExpectedReturns
+# PSO Update Step
+def updateVelocityAndWeights(intertiaWeight, Velocity, Weights, phi1, phi2, pBestPosition, glBestPosition,
+                              numPorfolios, numStocksInPort, absoluteMoveLimit):
+    # Update velocity and weights based on PSO equations
+    return [newWeights, newVelocity]
 
-# Calculate Variance-Covariance Matrix
-numPeriods = len(ExcessReturns)
-VarCov = ExcessReturns.T.dot(ExcessReturns) / (numPeriods - 1)
+# Iterations and Best Portfolio Display
+def displayGlobalBest(glBestFitValue, glBestPosition, numStocksInPort, printDims, StockTickers, expectedReturn,
+                      riskFreeRate, evalGoal, numPeriods=numPeriods, MONTHS_IN_YEAR=MONTHS_IN_YEAR):
+    # Display key summary statistics and optimal weights
+    pass  # Print statements and details here
+
+# Plot Optimal Portfolio Performance
+def plotOptimalPerformance(RawStockDataPivot, OptimalWeights):
+    # Calculate and plot the weighted prices of the optimal portfolio
+    pass  # Plotting code here
